2e94b9ebd79f2064bc89a0b809411a661607b796
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-03-27 00:48:16 +0100

    klaar met oefenen

---
+++ app/Http/Controllers/SolarSystemController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SolarSystem;
+use Illuminate\Http\Request;
+
+class SolarSystemController extends Controller
+{
+    public function index(){
+        $solarSystems = SolarSystem::with('planets')->get();
+        dd($solarSystems);
+        return view('solar_system.index', compact('solarSystems'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\PlanetController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SolarSystemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/solar-system',[SolarSystemController::class, 'index']);
+//Route::get('/solar-system/{solarSystem}',[SolarSystemController::class, 'show']);
+
+Route::get('/planets',[PlanetController::class, 'index']);
+//Route::get('/planets/{planet}',[PlanetController::class, 'show']);
